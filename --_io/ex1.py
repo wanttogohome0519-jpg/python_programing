92e28d17e022f20f82076c6366686786b288524c
wanttogohome0519-jpg <wanttogohome0519@gmail.com>
2026-08-20 15:23:19 +0900

    add 00_01/ex1.py

---
+++ --_io/ex1.py
@@ -0,0 +1,25 @@
+a=input()
+print(a, end="")
+print(type(a))
+print(a, type(a),sep="")
+
+
+a=input()
+a=int(a)
+print(a,type(a))
+int(input())
+print(a,type(a))
+
+b=float(input())
+print(b,type(b))
+
+a=int(input())
+b=int(input())
+a=input().split()
+print(a,type(a))
+
+map(int,input().split())
+a,b,c=map(int,input().split())
+print(a,type(a))
+a=list(map(int,input().spit()))
+print(a,type(a))
